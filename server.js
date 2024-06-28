d5411e5dfd182b390872c2c9597ca850f4fe31b0
Pinchuk Kirill <zoom.log221@gmail.com>
2024-06-28 14:56:07 +0800

    Add static and dynamic

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Привет, Октагон!</h1>');
+app.get('/static', (req, res) => {
+  res.json({header: "Hello", body: "Octagon NodeJS Test"});
+});
+
+app.get('/dynamic', (req, res) => {
+  const a = parseFloat(req.query.a);
+  const b = parseFloat(req.query.b);
+  const c = parseFloat(req.query.c);
+
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    res.json({ header: 'Error' });
+  } else {
+    const result = (a * b * c) / 3;
+    res.json({ header: 'Calculated', body: result.toString() });
+  }
 });
 
 app.listen(port, () => {
